4aea2840b3d35e7c4f10482b7ec516656b06a5e0
Claes Jakobsson <claes@surfar.nu>
2011-07-21 14:55:13 +0200

    Initial import from SVN

---
+++ lib/HTML/Selector/XPath/jQuery.pm
@@ -0,0 +1,54 @@
+package HTML::Selector::XPath::jQuery;
+
+use strict;
+use warnings;
+
+use base qw(HTML::Selector::XPath);
+
+my %INPUT_TYPE = (
+    checkbox    => 1,
+    file        => 1,
+    image       => 1,
+    password    => 1,
+    radio       => 1,
+    reset       => 1,
+    submit      => 1,
+    text        => 1,
+);
+
+sub parse_pseudo {
+    my ($self, $pseudo) = @_;
+
+    if (exists $INPUT_TYPE{$pseudo}) {
+        return "[\@type='${pseudo}']";
+    }
+
+    if ($pseudo =~ m{contains\((.*)}) {
+        my $text = $1;
+        if (defined $_[2] && $_[2] =~ /(.*?)\)/) {
+            if (defined $1) {
+                my $got = $1;
+                $_[2] =~ s/$1\)//;
+                $text .= $got;
+            }
+        }
+        else {
+            chop $text if substr($text, -1, 1) eq ")";
+        }
+        
+        $text = substr($text, 1, -1) if $text =~ /^'.*'$/;
+        return qq{[contains(., '$text')]};
+    }
+    
+    return;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::Selector::XPath::jQuery - HTML::Selector::XPath subclass with support 
+for some of the :selectors from jQuery
+
+=cut
---
+++ t/09-html-selector-xpath-jquery.t
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More qw(no_plan);
+
+BEGIN { use_ok("HTML::Selector::XPath::jQuery") }
+
+for (qw(checkbox file image password radio reset submit text)) {
+    is(
+        HTML::Selector::XPath::jQuery->new(":$_")->to_xpath,
+        qq{//[\@type='$_']},
+    );
+}
+
+is(
+    HTML::Selector::XPath::jQuery->new("div:contains(Företag)")->to_xpath,
+    q{//div[contains(., 'Företag')]}
+);
+
+is(
+    HTML::Selector::XPath::jQuery->new(q{input[name='man']})->to_xpath,
+    q{//input[@name='man']}
+)
